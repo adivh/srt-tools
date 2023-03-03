842f74c3d39fb476c6296b9224b3508e5902a2ff
adivh <adrian.vonheyl@gmail.com>
2023-03-03 12:21:09 +0100

    Added flexible document width

--- srt.py
+++ srt.py
@@ -7,35 +7,56 @@ class SRT:
         self.speakers = []
         self.original_texts = []
         self.translated_texts = []
-        
-        if len(srt_document) % 4 != 0:
-            raise Exception("Document has no valid structure")
+        self.column_count = 0
 
-        for i in range(0, int(len(srt_document) / 4)):
-            index = i * 4
+        _times_found = 0
+        for i in range(0, int(len(srt_document))):
+
+            if self._convert_time(srt_document.items[i], test_only=True):
+                _times_found = _times_found + 1
+
+            if _times_found > 1:
+                break
+
+            self.column_count = self.column_count + 1
+            
+
+        print(self.column_count)
+        
+        if len(srt_document) % self.column_count != 0:
+            raise Exception("Document has no valid structure: ({})".format(self.column_count))
+
+        for i in range(0, int(len(srt_document) / self.column_count)):
+            index = i * self.column_count
             self.times.append(self._convert_time(srt_document.items[index]))
             self.speakers.append(srt_document.items[index + 1])
             self.original_texts.append(srt_document.items[index + 2])
-            self.translated_texts.append(srt_document.items[index + 3])
+            self.translated_texts.append(srt_document.items[index + self.column_count - 1])
 
-    def _convert_time(self, time):
+    def _convert_time(self, time, test_only=False):
         reg = re.compile("[0-9]{2}[:][0-9]{2}[:][0-9]{2}[:][0-9]*")
         values = time.split(" ")
 
         if len(values) < 2:
-            print("faulty time: {}".format(time))
+            if test_only:
+                return False
             raise ValueError(time)
 
         res = reg.fullmatch(values[0])
         if res is None:
-            print("faulty time: {}".format(time))
+            if test_only:
+                return False
             raise ValueError(time)
 
         res = reg.fullmatch(values[1])
         if res is None:
-            print("faulty time: {}".format(time))
+            if test_only:
+                return False
             raise ValueError(time)
 
+        if test_only:
+            return True
+        
         return "{},{:0<3} --> {},{:0<3}".format(values[0][:8], values[0][9:], values[1][:8], values[1][9:])
 
     def __str__(self):
